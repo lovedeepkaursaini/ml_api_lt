4af98b6d229fdfcbd2eec56526b6faedd86bf1e3
Lovedeep Saini <lksaini@gbtpa.ajgco.com>
2020-05-20 14:09:37 -0500

    ab tests

--- backend/server/apps/ml/tests.py
+++ backend/server/apps/ml/tests.py
@@ -2,12 +2,60 @@
 import inspect
 from apps.ml.registry import MLRegistry
 
-# ...
-# the rest of the code
-# ...
+from django.test import TestCase
 
-# add below method to MLTests class:
-def test_registry(self):
+from apps.ml.income_classifier.random_forest import RandomForestClassifier
+from apps.ml.income_classifier.extra_trees import ExtraTreesClassifier
+
+class MLTests(TestCase):
+    def test_rf_algorithm(self):
+        input_data = {
+            "age": 37,
+            "workclass": "Private",
+            "fnlwgt": 34146,
+            "education": "HS-grad",
+            "education-num": 9,
+            "marital-status": "Married-civ-spouse",
+            "occupation": "Craft-repair",
+            "relationship": "Husband",
+            "race": "White",
+            "sex": "Male",
+            "capital-gain": 0,
+            "capital-loss": 0,
+            "hours-per-week": 68,
+            "native-country": "United-States"
+        }
+        my_alg = RandomForestClassifier()
+        response = my_alg.compute_prediction(input_data)
+        self.assertEqual('OK', response['status'])
+        self.assertTrue('label' in response)
+        self.assertEqual('<=50K', response['label'])
+
+# add new test method to MLTests class
+    def test_et_algorithm(self):
+        input_data = {
+            "age": 37,
+            "workclass": "Private",
+            "fnlwgt": 34146,
+            "education": "HS-grad",
+            "education-num": 9,
+            "marital-status": "Married-civ-spouse",
+            "occupation": "Craft-repair",
+            "relationship": "Husband",
+            "race": "White",
+            "sex": "Male",
+            "capital-gain": 0,
+            "capital-loss": 0,
+            "hours-per-week": 68,
+            "native-country": "United-States"
+        }
+        my_alg = ExtraTreesClassifier()
+        response = my_alg.compute_prediction(input_data)
+        self.assertEqual('OK', response['status'])
+        self.assertTrue('label' in response)
+        self.assertEqual('<=50K', response['label'])
+
+    def test_registry(self):
         registry = MLRegistry()
         self.assertEqual(len(registry.endpoints), 0)
         endpoint_name = "income_classifier"
